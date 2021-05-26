7fb563f21f8808ce04860301a365a0ecb6775aee
yoimar <urbina.yoimar@gmail.com>
2021-05-26 09:51:59 -0400

    Course Completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,28 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 0,
+    // 'vue/valid-template-root': 0,
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     singleQuote: true,
+    //     parser: 'flow',
+    //     endOfLine: 'auto',
+    //     usePrettierrc: false,
+    //     fileInfoOptions: {
+    //       withNodeModules: true,
+    //     },
+    //   },
+    // ],
   },
-};
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,33 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventShow from '../views/EventShow.vue'
+import EventCreate from '../views/EventCreate.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true,
   },
-];
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
+  },
+]
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
-});
+})
 
-export default router;
+export default router
